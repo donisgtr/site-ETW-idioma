8152983dac4b1e918a3eb7e3c99cb0d8ff7cc310
donissousa <donisgtr@gmail.com>
2022-05-31 21:21:53 -0300

    aula 04

--- main.js
+++ main.js
@@ -1,46 +1,76 @@
 window.addEventListener('scroll', onScroll)
-
 onScroll()
+
 function onScroll() {
-  showNavOnScroll()
-  showBackToTopButtonOnScroll()
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute('id');
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove('active');
+
+  if (sectionBoundaries) {
+    menuElement.classList.add('active');
+  }
 }
 
 function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    navigation.classList.add('scroll');
   } else {
-    navigation.classList.remove('scroll')
+    navigation.classList.remove('scroll');
   }
 }
 
 function showBackToTopButtonOnScroll() {
-  if (scrollY > 550) {
-    backToTopButton.classList.add('show')
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show');
   } else {
-    backToTopButton.classList.remove('show')
+    backToTopButton.classList.remove('show');
   }
 }
 
 function openMenu() {
-  document.body.classList.add('menu-expanded')
+  document.body.classList.add('menu-expanded');
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded')
+  document.body.classList.remove('menu-expanded');
 }
 
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 700
+  duration: 1000
 }).reveal(`
   #home, 
   #home img, 
   #home .stats, 
   #services,
-  #services header,
-  #services .card
-  #about, 
-  #about header, 
-  #about .content`)
+  #services header, 
+  #services .card,
+  #about,
+  #about header,
+  #about .content,
+  #contact header,
+  #contact .content
+`);
